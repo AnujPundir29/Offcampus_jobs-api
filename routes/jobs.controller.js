fe652db5482dbc76965d651805080cf42b1f094f
AnujPundir29 <pundiranuj28@gmail.com>
2021-12-04 04:43:54 +0530

    added more sources for jobs

--- routes/jobs.controller.js
+++ routes/jobs.controller.js
@@ -5,6 +5,17 @@ const serverCache = new NodeCache();
 
 const jobs = [];
 
+const platforms = [{
+        source: 'studentscircles',
+        address: 'https://www.studentscircles.com/category/it-jobs/',
+    },
+    {
+        source: 'offcampusjobs4u',
+        address: 'https://www.offcampusjobs4u.com/',
+    },
+];
+
+
 async function getUrlandTitle(URL, param) {
     const response = await axios.get(URL);
 
@@ -28,39 +39,49 @@ async function getUrlandTitle(URL, param) {
     return result;
 }
 
-async function httpGetAllJobs(req, res) {
+async function loadAllJobs() {
     try {
-        if (serverCache.has('allJobs')) {
-
-            console.log('Retrieved value from cache !!')
-            res.json(serverCache.get('allJobs'));
-        } else {
-            const WEB_URL = 'https://www.studentscircles.com/category/it-jobs/';
+        for (let j = 0; j < platforms.length; j++) {
+            const platform = platforms[j];
+            const WEB_URL = platform.address;
             const jobsData = await getUrlandTitle(WEB_URL, 'a:contains("Off Campus")');
 
             for (var i = 0; i < jobsData.length; i++) {
                 const title = jobsData[i].attribs.title + '';
-                const url = jobsData[i].attribs.href;
+                const url = jobsData[i].attribs.href + '';
                 var result = jobs.filter(x => x.title === title);
                 if (result.length === 0 && title !== "undefined") {
-                    const id = title.split(' ')[0];
+                    const id = title.split(' ')[0].replace(/\s/g, '');
                     jobs.push({
                         id,
                         title,
                         url,
-                        apply: 'https://offcampus-job.herokuapp.com/jobs/' + id,
-                        // apply: 'http://localhost:8000/jobs/' + id,
+                        source: platform.source,
+                        // apply: 'https://offcampus-job.herokuapp.com/jobs/' + id,
+                        apply: 'http://localhost:8000/jobs/' + id,
                     });
                 }
             }
+        }
+    } catch (error) {
+        console.error(error);
+        res.send('Some error there !!');
+    }
+}
 
+async function httpGetAllJobs(req, res) {
+    try {
+        if (serverCache.has('allJobs')) {
+            console.log('Retrieved value from cache !!')
+            res.json(serverCache.get('allJobs'));
+        } else {
             serverCache.set('allJobs', jobs)
             console.log('Value not present in cache, performing computation')
             res.json(jobs);
         }
-
     } catch (error) {
         console.log(error);
+        res.send('Some error there !!');
     }
 }
 
@@ -73,8 +94,13 @@ async function httpGetJobById(req, res) {
         } else {
             const job = jobs.find(job => job.id === id);
             const JOB_URL = job.url;
-            const applyData = await getUrlandTitle(JOB_URL, '.dblclick_btn');
-
+            const jobSource = job.source;
+            let applyData;
+            if (jobSource === 'studentscircles') {
+                applyData = await getUrlandTitle(JOB_URL, '.dblclick_btn');
+            } else if (jobSource === 'offcampusjobs4u') {
+                applyData = await getUrlandTitle(JOB_URL, 'a:contains("Click")');
+            }
             const url = applyData[0].attribs.href;
 
             console.log('not in cache');
@@ -84,11 +110,13 @@ async function httpGetJobById(req, res) {
 
     } catch (error) {
         console.log(error);
+        res.send('Some error there !!');
     }
 
 }
 
 module.exports = {
+    loadAllJobs,
     httpGetAllJobs,
     httpGetJobById
 }
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const helmet = require('helmet');
 const jobsRouter = require('./routes/jobs.router');
+const {loadAllJobs} = require('./routes/jobs.controller');
 const PORT = process.env.PORT || 8000;
 
 app.use(helmet());
@@ -9,9 +10,14 @@ app.use(helmet());
 app.use('/jobs', jobsRouter);
 
 app.get('/*', (req, res) => {
-    res.json('Welcome to homepage!!. Please use /jobs to get jobs list and also use json viewer in browser'); 
-})
-
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    res.json('Welcome to homepage!!. Please use /jobs to get jobs list and also use json viewer in browser');
 });
+
+async function startServer() {
+    await loadAllJobs();
+    app.listen(PORT, () => {
+        console.log(`Server is listening on port ${PORT}`);
+    });
+}
+
+startServer();
