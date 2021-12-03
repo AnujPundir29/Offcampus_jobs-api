75ea97cdb68ab29ff87d0afe8d0c219cfd34dcbc
AnujPundir29 <pundiranuj28@gmail.com>
2021-12-03 14:39:33 +0530

    homepage

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
 const jobsRouter = require('./routes/jobs.router');
-const PORT = process.env.PORT ||  8000;
+const PORT = process.env.PORT || 8000;
 
 app.use('/jobs', jobsRouter);
+
+app.get('/*', (req, res) => {
+    res.json('Welcome to homepage')
+})
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
